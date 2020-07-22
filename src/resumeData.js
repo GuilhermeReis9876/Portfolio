80f1b987c034b16a93b5a154f8a6a98cb5361d22
GuilhermeReis9876 <guilhermereis9876@gmail.com>
2020-07-21 23:30:11 -0300

    Initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,84 @@
+let resumeData = {
+    "imagebaseurl": "https://rbhatia46.github.io/",
+    "name": "Guilherme Reis",
+    "role": "Web and Mobile Developer",
+    "linkedinId": "Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I currently work with React, React Native and C# on the backend. I love studying new technologies, read technical articles or simply play games in my free time.",
+    "socialLinks": [{
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/guilhermereis9876/",
+            "className": "fa fa-linkedin"
+        },
+        {
+            "name": "github",
+            "url": "https://github.com/GuilhermeReis9876",
+            "className": "fa fa-github"
+        },
+    ],
+    "aboutme": "I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address": "Belo Horizonte, Brazil",
+    "website": "https://guilhermereis.dev.com",
+    "education": [{
+            "UniversityName": "Information Systems - Pitágoras",
+            "specialization": "Some specialization",
+            "MonthOfPassing": "Aug",
+            "YearOfPassing": "2020",
+            "Achievements": "Some Achievements"
+        },
+        {
+            "UniversityName": "Computer Technician - Senai",
+            "specialization": "Some specialization",
+            "MonthOfPassing": "Jan",
+            "YearOfPassing": "2018",
+            "Achievements": "Some Achievements"
+        }
+    ],
+    "work": [{
+            "CompanyName": "SporTI",
+            "specialization": "Some specialization",
+            "MonthOfLeaving": "Jan",
+            "YearOfLeaving": "2018",
+            "Achievements": "Some Achievements"
+        },
+        {
+            "CompanyName": "ConsuTI",
+            "specialization": "Intern developer systems",
+            "MonthOfLeaving": "Jan",
+            "YearOfLeaving": "2018",
+            "Achievements": "Some Achievements"
+        }
+    ],
+    "portfolio": [{
+            "name": "project1",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/phone.jpg"
+        },
+        {
+            "name": "project2",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/project.jpg"
+        },
+        {
+            "name": "project3",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/project2.png"
+        },
+        {
+            "name": "project4",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/phone.jpg"
+        }
+    ],
+    "testimonials": [{
+            "description": "Um ótimo profissional",
+            "name": "Cristian Gomes - CEO SporTI"
+        },
+        {
+            "description": "This is a sample testimonial",
+            "name": "Some technical guy"
+        }
+    ]
+}
+
+export default resumeData
